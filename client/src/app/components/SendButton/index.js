faa7aa3735463f1a312fd957b2e63573170a9c30
OInVein <progthrash3305@gmail.com>
2021-07-16 23:01:16 +0800

    fix wrong pseudo content

--- client/src/app/components/SendButton/index.js
+++ client/src/app/components/SendButton/index.js
@@ -10,7 +10,7 @@ const SendButton = () => {
     <div
       className="
         w-12 h-12 motion-safe:hover:scale-110 transition text-xs cursor-pointer bottom-0 right-0 absolute bg-orange-400 rounded-lg flex justify-center items-center
-        after:content-['···'] after:w-6 after:h-6 after:border-2 after:border-white after:border-solid after:rounded-full after:rounded-bl-none after:text-white after:flex after:items-center after:justify-center after:absolute
+        after:content-['⋯'] after:w-6 after:h-6 after:border-2 after:border-white after:border-solid after:rounded-full after:rounded-bl-none after:text-white after:flex after:items-center after:justify-center after:absolute
         tracking-tighter
       "
       data-testid="sendButton"
